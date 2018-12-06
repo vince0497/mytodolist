1fe591238f62aed5cfc16bbf4903100181a6b1a7
Biben <brincellopez@gmail.com>
2018-12-06 14:07:46 +0800

    init

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import "./App.css";
+import List from "./List";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: "",
+      items: []
+    };
+  }
+
+  /*componentDidMount() {
+    getList().then(data => {
+      //console.log(data);
+
+      this.setState({
+        items: [...this.state.items, ...data]
+      });
+    });
+  }*/
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <h1 className="text-center">To do List</h1>
+            <h5 className="text-center">Brincel Lopez</h5>
+            <List />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+
+var index = require("./routes/index");
+var tasks = require("./routes/tasks");
+
+const cors = require("cors");
+
+var port = 5000;
+
+var app = express();
+//http://localhost:3000/
+//https://todolistbrincel.herokuapp.com/
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true
+  })
+);
+
+
+
+//View Engine
+app.set("views", path.join(__dirname, "client/public"));
+app.set("view engine", "ejs");
+app.engine("html", require("ejs").renderFile);
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "client")));
+
+// Body Parser MW
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/", index);
+
+app.use("/api", tasks);
+
+
+
+
+app.listen(process.env.PORT ||port, function() {
+  console.log("Server started on port " + port);
+});
